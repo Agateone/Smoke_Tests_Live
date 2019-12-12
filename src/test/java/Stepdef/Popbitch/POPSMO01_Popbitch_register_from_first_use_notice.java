3df63ff00018ea97bc222c7413900a3dda26011a
Agateone <jayasree.kamineni1@gmail.com>
2019-12-12 12:35:41 +0000

    first commit

--- src/test/java/Stepdef/Popbitch/POPSMO01_Popbitch_register_from_first_use_notice.java
+++ src/test/java/Stepdef/Popbitch/POPSMO01_Popbitch_register_from_first_use_notice.java
@@ -74,14 +74,14 @@ public class POPSMO01_Popbitch_register_from_first_use_notice {
 						Popbitch_First_Use_Notice_Elements popbitch_first_use_elements= new Popbitch_First_Use_Notice_Elements(driver);
 						Thread.sleep(5000);
 						popbitch_first_use_elements.Click_On_Popbitch_First_Use_Notice_Create_Wallet();
-						Thread.sleep(10000);
+						/*Thread.sleep(10000);
 						WebDriverWait wait = new WebDriverWait(driver, 20);
 						wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("email")));	
 						String reg_Page_url= driver.getCurrentUrl();
 						if(reg_Page_url.contains("https://account.agate.io/my-agate/sign-up?"))
 						{
 							System.out.println("Clicking on create wallet opened registration page");
-						}
+						}*/
 				} 
 				//edge
 					else if (browser.equalsIgnoreCase("Edge")) {
